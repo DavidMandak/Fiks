bca2744028c72249bb44b9c18159cec136b0f813
DavidMandak <dejf.mandak@gmail.com>
2025-01-24 06:52:32 +0100

    Almost works

--- 11. ročník/Programs/Kolo_4-1.py
+++ 11. ročník/Programs/Kolo_4-1.py
@@ -36,8 +36,36 @@ for problem in lines[1:]:
             positive, negative, row = diagonals(-1, pos, layer, start, end)
         tiles.append([pos, positive, negative, row])
     check = [0]*3
+    h = None
+    corners = []
     for line in range(1, 4):
         for i in range(3):
+            control = tiles[i][line]
             for tile in tiles[:i]+tiles[i+1:]:
-                pass
-    exit()
+                if control == tile[line]:
+                    check[i] += 1
+                    if line == 3:
+                        corners.append(tile[1]+tile[2])
+                        h = tile[3]
+    if  len(corners) == 2 and all(n == 2 for n in check):
+        dist = abs(corners[0]-corners[1])//2
+        if not dist % 3:
+            width = (corners[0]+corners[1])//2
+            height = h+(dist//3)
+            if height >= abs(width)+2:
+                lay = height
+                if lay % 2:
+                    k = 1
+                else:
+                    k = -1
+                print(lay*(3*lay-1)//2+1+lay+(lay+k*width)//2, file=output)
+            else:
+                lay = (abs(width)+height)//2
+                if (width < 0 and lay % 2) or (width > 0 and not lay % 2):
+                    print(lay*(3*lay-1)//2+1+height, file=output)
+                else:
+                    print((lay+1)*(3*(lay+1)-1)//2-height, file=output)
+        else:
+            print(0, file=output)
+    else:
+        print(-1, file=output)
--- 11. ročník/Programs/test.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-a = defaultdict(int)
-
-b = [0, 1, 2, 0, 2, 5]
-for c in b:
-    a[c] += 1
-print(a)
