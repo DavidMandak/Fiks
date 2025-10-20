38f8a3d06138079fcff33aa1017d7bc06730d8ef
DavidMandak <dejf.mandak@gmail.com>
2025-10-20 19:59:30 +0200

    Deleting ročník 11.

--- 11. ročník/Programs/Kolo_5-2.py
+++
@@ -1,7 +0,0 @@
-lines = open("../test.txt").read().splitlines()
-
-pos = 1
-for _ in range(int(lines[0])):
-    r, s = map(int, lines[pos].split())
-    sectors = list(map(int, lines[pos+1]))
-    robots =
--- 11. ročník/Programs/test1.py
+++
@@ -1,8 +0,0 @@
-a = (1+(1+24*(int(input())-1))**(1/2))//6
-b = (1+(1+24*(int(input())-1))**(1/2))//6
-if a > b:
-    print("a")
-elif b > a:
-    print("b")
-else:
-    print("gg")
